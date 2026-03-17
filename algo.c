a32916939efab32b170bb52912568e00ed1c702e
Salamu2007 <boukhendegnour@gmail.com>
2026-03-17 12:27:04 +0100

    fix mongodb atlas

--- algo.c
+++ algo.c
@@ -1,29 +1,73 @@
 #include <stdio.h>
 
-int power(int x, int n){
-    if(n == 0)
-        return 1;
-    else if(n == 1)
-        return x;
+#define MAX 100
 
-    int half = power(x, n/2);
+void merge(int arr[], int left, int mid, int right)
+{
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    int temp[MAX];
 
-    if(n % 2 == 0)
-        return half * half;
-    else
-        return x * half * half;
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right)
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (i = left; i <= right; i++)
+    {
+        arr[i] = temp[i];
+    }
 }
 
-int main(){
-    int x, n;
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
 
-    printf("Enter x: ");
-    scanf("%d", &x);
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
 
-    printf("Enter n: ");
-    scanf("%d", &n);
+        merge(arr, left, mid, right);
+    }
+}
 
-    printf("Result = %d", power(x, n));
+int main()
+{
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
